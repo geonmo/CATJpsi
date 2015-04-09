b169611d4528d4b1aa1ba37ebe2633e9b366e924
Geonmo <ry840901@gmail.com>
2015-04-09 20:45:51 +0900

    Analysis code about j/psi's muon decay channel.

--- JPsiAnalysis/test/ana_decay.py
+++ JPsiAnalysis/test/ana_decay.py
@@ -18,163 +18,147 @@ class DecayAna(AbsAna) :
   hist={}
   def __init__( self, infile, outfile ) :
     AbsAna.__init__(self,infile, outfile)
+    self.type_list ={ 1:"GENMu", 2:"CATMu" , 3:"PFMu" , 4:"GLBMu" , 5:"TRKMu", 6:"STDMu",7:"GTrk",8:"CJpsi" }
 
   def BookingHist( self,output ) :
-    for muon_type in ['gen','cat','pf','global','tracker'] :
+    for muon_type in self.type_list.values() : 
       self.hist[muon_type+'_muon_pt'  ] = TH1F (muon_type+"_muon_pt",   muon_type+"_muon_pt; Muon pT(GeV/c); Entries",     100,0,200)
       self.hist[muon_type+'_muon_eta' ] = TH1F (muon_type+"_muon_eta",  muon_type+"_muon_eta",                             100,-5,5)
       self.hist[muon_type+'_muon_phi' ] = TH1F (muon_type+"_muon_phi",  muon_type+"_muon_phi",                             100,-math.pi,math.pi)
-      self.hist[muon_type+'_muon_mass'] = TH1F (muon_type+"_muon_mass", muon_type+"_muon_mass;Muon mass(GeV/c^2);Entries", 100,0.1,0.11)
+      self.hist[muon_type+'_muon_mass'] = TH1F (muon_type+"_muon_mass", muon_type+"_muon_mass;Muon mass(GeV/c^{2});Entries", 100,0.08,0.12)
+      self.hist[muon_type+'_muon_entries'] = TH1F (muon_type+"_nMuons", muon_type+"_nMuons", 4,0,4)
+    self.hist['decayed_muon_type'] = TH1F("decayed_muon_type","Found muon's type from J/#psi;Muon type;Entries(Total)",8,1,9)
+    for idx, type in enumerate(self.type_list.values()) :
+      self.hist['decayed_muon_type'].GetXaxis().SetBinLabel(1+idx,type)
+    self.hist['nMuons'] = TProfile("n_Muon","Number of found Muons per event",9,1,10)
+    self.hist['nMuons'].GetXaxis().SetBinLabel(1,"Total")
+    for idx, type in enumerate(self.type_list.values()) :
+      self.hist['nMuons'].GetXaxis().SetBinLabel(idx+2,type)
+  
 
-  def FillHist( self, muon, muon_type="gen") :
-    self.hist[(muon_type+"_muon_pt")].Fill( muon.Pt())
-    self.hist[(muon_type+"_muon_eta")].Fill( muon.Eta() )
-    self.hist[(muon_type+"_muon_phi")].Fill( muon.Phi() )
-    self.hist[(muon_type+"_muon_mass")].Fill( muon.M() )
+  def FillHist( self, muon, muon_type="GENMu") :
+    #print muon_type, muon.pt(), muon.eta(), muon.phi,muon.mass()
+    self.hist[muon_type+"_muon_pt"].Fill( muon.pt())
+    self.hist[muon_type+"_muon_eta"].Fill( muon.eta() )
+    self.hist[muon_type+"_muon_phi"].Fill( muon.phi() )
+    self.hist[muon_type+"_muon_mass"].Fill( muon.mass() )
+
+    for idx, type in self.type_list.iteritems() :
+      if ( muon_type == type) :
+        self.hist['decayed_muon_type'].Fill( idx )
     
   def Ana( self ) :
-    file = TFile.Open(self.inputfile)
-    print "Load file : "+file.GetName()
-    if ( file.IsZombie()) :
-      print "%s is corruct!"%(self.inputfile)
-      return
-    file.Close()
-
-    outfile = TFile(self.outfilename,"RECREATE")
-    for mode in c_modes :
-      for x in steps :
-        outfile.mkdir("%s_s%d"%(mode,x))
+    output = TFile(self.outfile,"RECREATE")
     self.BookingHist(outfile)
 
-    events = Events(self.inputfile)
+    events = self.events
 
     goodVTXLabel, GVTX = "goodOfflinePrimaryVertices", Handle("std::vector<reco::Vertex>")
-    catMuonLabel, catMuon = "catMuons", Handle("std::vector<cat::Muon>")
+    catMuonLabel, catMuonHandle = "catMuons", Handle("std::vector<cat::Muon>")
+    genParticleLabel, genParticle = "genParticles", Handle("vector<reco::GenParticle>")
+    pfAllMuonLabel, pfAllMuon = "pfAllMuonsPFlow", Handle("vector<reco::PFCandidate>")
+    recoMuonLabel, recoMuon = "muons", Handle("vector<reco::Muon>")
+    patTrackCandLabel, patTrackCand = "patTrackCandsPFlow", Handle("vector<pat::GenericParticle>")
+    associatedpfAllMuonLabel, assopfAllMuon =  "pfAllMuonsGenParticlesMatch" , Handle("edm::Association<vector<reco::GenParticle> >")
+    catJpsiLabel, catJpsiHandle = "catSecVertexs", Handle("std::vector<cat::SecVertex>")    
+
+
     for iev,event in enumerate(events):
       event.getByLabel(goodVTXLabel, GVTX)
-      event.getByLabel(catMuonLabel, catMuon)
-      event.getByLabel(catElectronLabel, catElectron)
-      event.getByLabel(catSecVertexLabel, catSecVtx)
-      event.getByLabel(catJetLabel, catJet)
-
+      event.getByLabel(catMuonLabel, catMuonHandle)
+      event.getByLabel(catJpsiLabel, catJpsiHandle)
+      event.getByLabel(genParticleLabel, genParticle)
+      event.getByLabel(pfAllMuonLabel, pfAllMuon)
+      event.getByLabel(patTrackCandLabel, patTrackCand)
+      event.getByLabel(associatedpfAllMuonLabel, assopfAllMuon )
+      event.getByLabel(recoMuonLabel,recoMuon )
+      
+      
+      catJpsis = None
       goodvtx = GVTX.isValid()
-      secVtx = None
       try :      
-        muons = catMuon.product()
-        electrons = catElectron.product()
-        jets = catJet.product()
+        genParticles = genParticle.product()
+        recoMuons = recoMuon.product()
       except RuntimeError :
         print iev 
         print "Warning! Among of one's product is null."
         continue
-      if ( len(muons)<2 and len(electrons) <2) :
-        continue
       try :
-        secVtxs = catSecVtx.product()
+        pfAllMuons = pfAllMuon.product()
       except RuntimeError :
-        print "SecVtx is missing"
-        secVtxs =[]
-      #print "muon : %d electorns : %d jets : %d"%(len(muons),len(electrons),len(jets))
+        print iev
+        print "Warning! pfMuon is missing."
+        pfAllMuons =[]
+      try :
+        catMuons = catMuonHandle.product()
+      except RuntimeError :
+        print "Warning! Cat Muon."
+        catMuons =[]
 
-      muon_list =[]
-      elec_list =[]
-      jet_list  =[]
-      jpsi_list =[]
-      l1jpsi_list=[]
-      l2jpsi_list=[]
+      try : 
+        patTrackCands = patTrackCand.product()
+      except RuntimeError :
+        print iev
+        print "patTrackCand error"
+        patTrackCands =[]
 
-      #Acquiring POGs.
-      for muon in muons :
-        t_muon = Muon(muon,'m')
-        if ( t_muon.valid() ) :
-          muon_list.append( t_muon ) 
-      for elec in electrons :
-        t_elec = Electron(elec,'e')
-        if ( t_elec.valid() ) :
-          elec_list.append( t_elec )
-      for jet in jets :
-        t_jet = Jet(jet)
-        if ( t_jet.valid() ) :
-          jet_list.append(t_jet)
+      try :
+        catJpsis = catJpsiHandle.product()
+      except RuntimeError :
+        print "CAT Jpsi collection is empty or null ptr."
+        catJpsis =[]
 
-      ## J/psi's daughther type.
-      for secVtx in secVtxs :
-        if secVtx is None :
-          continue
-        if ( abs(secVtx.pdgId()) == 11 ) :
-          #t_sV = Jpsi(secVtx, electrons)
-          t_sV = Jpsi(secVtx)
-          jpsi_list.append( t_sV )
-        elif ( abs(secVtx.pdgId()) == 13 ) :
-          #t_sV = Jpsi(secVtx, muons)
-          t_sV = Jpsi(secVtx)
-          jpsi_list.append( t_sV ) 
-        else :
-          print "error."
+      gen_list =[]
+      #if ( genParticles is None) : continue
+      nMuon=[0,0,0,0,0,0,0,0,0]
+      nMuon[0] +=1
+      for gen in genParticles :
+        if (abs(gen.pdgId())==443 and gen.numberOfDaughters()==2) :
+          if ( abs(gen.daughter(0).pdgId())==13 and abs(gen.daughter(1).pdgId())==13) :
+            gen_list.append( gen.daughter(0))
+            gen_list.append( gen.daughter(1))
+            nMuon[1] +=2
+            self.FillHist( gen.daughter(0), "GENMu")
+            self.FillHist( gen.daughter(1), "GENMu")
+            for catJpsi in self.cleaning(catJpsis,False) :
+              if self.isEqual(gen, catJpsi,False) :
+                self.FillHist( gen.daughter(0), "CJpsi")
+                self.FillHist( gen.daughter(1), "CJpsi")
+                nMuon[8] +=2
+                
 
-      # Event Selection.
-      lep_list = muon_list + elec_list
-      # If num. of leptons < 2, it is not dilepton events.
-      if ( len(lep_list) <2 ) : continue
+      for gen in gen_list :
+        for pfmuon in pfAllMuons :
+          if ( self.isEqual( gen, pfmuon, False)) :
+            self.FillHist( pfmuon, "PFMu")
+            nMuon[3] +=1 
+        for generalTrack in patTrackCands :
+          if ( self.isEqual( gen, generalTrack, False)) :
+            self.FillHist( generalTrack, "GTrk")
+            nMuon[7] +=1 
+        for catmuon in catMuons :
+          if ( self.isEqual( gen, catmuon, False)) :
+            self.FillHist( catmuon, "CATMu") 
+            nMuon[2] +=1 
+        for recomuon in recoMuons :
+          if ( self.isEqual( gen, recomuon, False)) :
+            if ( recomuon.isStandAloneMuon()) :
+              self.FillHist( recomuon, "STDMu")
+              nMuon[6] +=1 
+            if ( recomuon.isTrackerMuon()) :
+              self.FillHist( recomuon,"TRKMu")
+              nMuon[5] +=1 
+            if ( recomuon.isGlobalMuon()) :
+              self.FillHist( recomuon,"GLBMu")
+              nMuon[4] +=1
+     
+      for x in range(9)[1:] :
+        self.hist['nMuons'].Fill(x+1,nMuon[x])
+        self.hist[self.type_list[x]+'_muon_entries'].Fill( nMuon[x])
+        
 
-      # Cut Flow
-      pass_flag={}
-      for mode in c_modes :
-        pass_flag[mode] =[False,False,False,False,False]
-      for lep_idx, iso_lep1 in enumerate(lep_list) :
-        for iso_lep2 in lep_list[lep_idx:] :
-          mode =""
-          if ( iso_lep1.type=='m' and iso_lep2.type=='m') :
-            mode="MuMu"
-          elif ( iso_lep1.type=='e' and iso_lep2.type=='e') :
-            mode="ElEl"     
-          elif ( iso_lep1.type=='m' and iso_lep2.type=='e') :
-            mode="MuEl"
-          
-          #To make required POGs.
-          ### Isolated lepton removed Jet.
-          cleaned_jet_list =[]
-          JpsiWithDR_5 = []
-          JpsiWithvProb_dPV = []
-          for jet in jet_list :
-            if ( jet.jetCleaning( iso_lep1, iso_lep2) ) :
-              cleaned_jet_list.append( jet )
-          ### Jpsi with dR< 0.5
-          for jpsi in jpsi_list :
-            jpsi.JetDR(cleaned_jet_list)
-            if ( jpsi.minDR< 0.5) :
-              JpsiWithDR_5.append(jpsi)
-            if ( jpsi.vProb>0.01 and (jpsi.l3D>0.02 and jpsi.l3D<2) ) :
-              JpsiWithvProb_dPV.append( jpsi)
-               
-          #step1
-          dilepton_mass = (iso_lep1+iso_lep2).M()
-          if ( dilepton_mass<20. or iso_lep1.q* iso_lep2.q != -1 ) :
-            continue
-          pass_flag[mode][0] = True
-          #step2
-          if (dilepton_mass >76 and dilepton_mass < 106 and mode !="MuEl") :
-            continue
-          pass_flag[mode][1] = True
-          #step3
-          if ( len( cleaned_jet_list) <1 ) :
-            continue
-          pass_flag[mode][2] = True
-          #step4
-          if ( len(JpsiWithDR_5)<1 ) :
-            continue
-          pass_flag[mode][3] = True
-          if ( len(JpsiWithvProb_dPV) <1 ) :
-            continue
-          pass_flag[mode][4] = True
 
-      for mode in c_modes :
-        #print pass_flag[mode]
-        for idx,flag in enumerate(pass_flag[mode]) :
-          if ( flag ) : 
-            self.FillHist( mode, idx+1, muon_list,elec_list,jet_list,jpsi_list)
-
-    outfile.Write()
+    output.Write()
 
   def Print(self) :
     print self.inputfile, self.outfilename
@@ -182,5 +166,5 @@ class DecayAna(AbsAna) :
 if __name__ == "__main__" :
   infile = sys.argv[1]
   outfile = sys.argv[2]
-  ana = JpsiAna( infile, outfile )
+  ana = DecayAna( infile, outfile )
   ana.Ana()
